75f75bc1bd46a1158cddcb9cdecea28e7c47e4c6
Gabriel Anderson <zettablade@gmail.com>
2008-07-28 19:21:55 -0700

    Started constructing the initial design. Fairly useless at this point

---
+++ fgsfds/video/all.d
@@ -0,0 +1,23 @@
+module fgsfds.video.all;
+/**
+ * Imports all the files in fgsfds.video
+ *
+ * License:
+ *  This file is part of fgsfds.
+ *  Copyright (C) 2007-2008 by Gabriel Anderson zettablade@gmail.com
+ *
+ *  fgsfds is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  fgsfds is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+import fgsfds.video.Window;
